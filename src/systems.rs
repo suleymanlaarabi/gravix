119efa7baa5a596a6613a16c29dca595dc9d3355
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2025-03-30 19:00:46 +0200

    use hashset (improve performance)

--- src/systems.rs
+++ src/systems.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use avian3d::prelude::*;
 use bevy::prelude::*;
 
@@ -9,15 +11,14 @@ pub fn process_ground_start_sensor(
     query_ground: Query<Entity, With<Ground>>,
     mut collision_start_event_reader: EventReader<CollisionStarted>,
 ) {
+    let groundsensor_entities: HashSet<Entity> = query_groundsensor.iter().collect();
+    let ground_entities: HashSet<Entity> = query_ground.iter().collect();
+
     for CollisionStarted(c1, c2) in collision_start_event_reader.read() {
-        if query_groundsensor.contains(*c1) {
-            if query_ground.contains(*c2) {
-                commands.entity(*c1).insert(OnGround);
-            }
-        } else if query_groundsensor.contains(*c2) {
-            if query_ground.contains(*c1) {
-                commands.entity(*c2).insert(OnGround);
-            }
+        if groundsensor_entities.contains(c1) && ground_entities.contains(c2) {
+            commands.entity(*c1).insert(OnGround);
+        } else if groundsensor_entities.contains(c2) && ground_entities.contains(c1) {
+            commands.entity(*c2).insert(OnGround);
         }
     }
 }
@@ -28,15 +29,14 @@ pub fn process_ground_end_sensor(
     query_ground: Query<Entity, With<Ground>>,
     mut collision_ended_event_reader: EventReader<CollisionEnded>,
 ) {
+    let groundsensor_entities: HashSet<Entity> = query_groundsensor.iter().collect();
+    let ground_entities: HashSet<Entity> = query_ground.iter().collect();
+
     for CollisionEnded(c1, c2) in collision_ended_event_reader.read() {
-        if query_groundsensor.contains(*c1) {
-            if query_ground.contains(*c2) {
-                commands.entity(*c1).remove::<OnGround>();
-            }
-        } else if query_groundsensor.contains(*c2) {
-            if query_ground.contains(*c1) {
-                commands.entity(*c2).remove::<OnGround>();
-            }
+        if groundsensor_entities.contains(c1) && ground_entities.contains(c2) {
+            commands.entity(*c1).remove::<OnGround>();
+        } else if groundsensor_entities.contains(c2) && ground_entities.contains(c1) {
+            commands.entity(*c2).remove::<OnGround>();
         }
     }
 }
